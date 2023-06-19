85ed1cef2fae7dbd1a96289b3b2c34ee01e795ad
CongPhiNguyen <congphinguyen312@gmail.com>
2023-06-19 23:31:55 +0700

    add chatvotri-bot

--- controllers/chatCtrl.js
+++ controllers/chatCtrl.js
@@ -1,6 +1,11 @@
 const natural = require("natural")
 const tokenizer = new natural.WordTokenizer()
 const vietnameseStopwords = require("vietnamese-stopwords")
+const dialogflow = require("dialogflow")
+const uuid = require("uuid")
+const sessionClient = new dialogflow.SessionsClient()
+const sessionPath = sessionClient.sessionPath(process.env.PROJECT_ID, uuid.v4())
+
 function getRandomValueFromArray(arr) {
   const randomIndex = Math.floor(Math.random() * arr.length)
   return arr[randomIndex]
@@ -18,6 +23,35 @@ const chatCtrl = {
       success: true,
       topic: prediction,
       reply: getRandomValueFromArray(global.reply[prediction])
+    })
+  },
+  chatGossip: async (req, res) => {
+    const { message } = req.body
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: message,
+          languageCode: "en-US"
+        }
+      }
+    }
+
+    const responses = await sessionClient.detectIntent(request)
+    console.log(responses)
+    const { queryResult } = responses[0]
+    // const dialogFlowFeature = await handleIntent(queryResult)
+
+    console.log(queryResult.fulfillmentText)
+    // const tokenizedNewText = tokenizer.tokenize(message)
+    // const filteredNewText = tokenizedNewText.filter(
+    //   (token) => !vietnameseStopwords.includes(token.toLowerCase())
+    // )
+    // const prediction = global.basicChatModel.classify(filteredNewText)
+    return res.status(200).send({
+      success: true,
+      // topic: prediction,
+      reply: queryResult.fulfillmentText
     })
   }
 }
--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -2,5 +2,6 @@ const router = require("express").Router()
 const chatCtrl = require("../controllers/chatCtrl")
 
 router.post("/chat-basic", chatCtrl.chatBasic)
+router.post("/chat-gossip", chatCtrl.chatGossip)
 
 module.exports = router
